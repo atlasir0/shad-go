177ba84fe22e0964f58f047c579a3c922834c89f
Rinat_K <rinat.kurmakaev@outlook.com>
2024-08-12 21:54:31 +0300

    solution of urlfetch

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,38 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+func main() {
+	if len(os.Args) < 1 {
+		fmt.Println("Error")
+		os.Exit(1)
+	}
+
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("Content of", url)
+		fmt.Println("----------------------------------------")
+		buf := make([]byte, 1024)
+		for {
+			n, err := resp.Body.Read(buf)
+			if n > 0 {
+				fmt.Print(string(buf[:n]))
+			}
+			if err != nil {
+				break
+			}
+		}
+		fmt.Println("----------------------------------------")
+	}
 }
